7490e3404e6acc525eb4167f200bc4fbef5e6933
AHealy16 <AH211841@reigate.ac.uk>
2022-11-24 11:59:41 +0000

    Updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,29 @@
 package com.company;
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] Sales = {};
+        ArrayList <Integer> Sales = new ArrayList<Integer>(5);
         Boolean w = true;
         Scanner input = new Scanner(System.in);
 
         while (w = true){
 
             System.out.println("Would you like to add a yearly income?");
-            if (input.next() == "No"){
+            String decision = input.next();
+            if (decision.equalsIgnoreCase("No")){
                 w = false;
                 System.out.println(Sales);
+                break;
             }
             System.out.println("Please enter a year where 0 is the first year");
             int year = input.nextInt();
             System.out.println("Please enter the revenue for that year");
             int income = input.nextInt();
-            Sales = income;
+            int array_Place = year - 1;
+            Sales.set(array_Place, income);
         }
 
     }
